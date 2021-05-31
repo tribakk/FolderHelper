47c4a45217870ab4c74ca4f0ce1de5d3fe93ec80
Павел Козлов <pavel.kozlov@fsight.ru>
2021-05-31 18:18:35 +0500

    передвинул и переименовал кнопку

--- FolderXmlParser/Form1.Designer.cs
+++ FolderXmlParser/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(275, 204);
+            this.button2.Location = new System.Drawing.Point(348, 51);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(129, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Распарсить папки";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
